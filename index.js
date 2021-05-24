feaad0d5cff248f6b885fa69f780c6477e733dd4
Tricia Brooker <grimmi5894@gmail.com>
2021-05-24 19:17:01 -0400

    create functions to validate gameCounts and positionCounts

--- index.js
+++ index.js
@@ -9,7 +9,24 @@ const getTeamCount = (lineup) => {
     teamCounts[player.teamId] = teamCounts[player.teamId] === undefined ? 1 : teamCounts[player.teamId]
 
     return teamCounts
-  })
+  }, {})
+}
+
+const getGameCount = (lineup) => {
+  return lineup.reduce((gameCounts, player) => {
+    gameCounts[player.gameId] = gameCounts[player.gameId] === undefined ? 1 : gameCounts[player.gameId]
+
+    return gameCounts
+  }, {})
+}
+
+const getPositionCount = (lineup) => {
+  return lineup.reduce((positionCounts, player) => {
+    positionCounts[player.position] = positionCounts[player.position] === undefined ? 1
+      : positionCounts[player.position]
+
+    return positionCounts
+  }, {})
 }
 
 const violatesSalary = (lineup) => {
@@ -20,10 +37,24 @@ const violatesTeamCount = (teamCounts) => {
   return Object.values(teamCounts).some((count) => { return count > 2 })
 }
 
+const violatesGameCount = (gameCounts) => {
+  return Object.values(gameCounts).some((count) => { return count > 3 })
+}
+
+const violatesPositionCount = (positionCounts) => {
+  return positionCounts['P'] !== 1 || positionCounts['C'] !== 1 ||
+  positionCounts['1B'] !== 1 || positionCounts['2B'] !== 1 ||
+  positionCounts['3B'] !== 1 || positionCounts['SS'] !== 1 ||
+  positionCounts['OF'] !== 3
+}
+
 const validateLineup = (lineup) => {
   const teamCounts = getTeamCount(lineup)
+  const gameCounts = getGameCount(lineup)
+  const positionCounts = getPositionCount(lineup)
 
-  return !violatesSalary(lineup) && !violatesTeamCount(teamCounts)
+  return !violatesSalary(lineup) && !violatesTeamCount(teamCounts) &&
+  !violatesGameCount(gameCounts) && !violatesPositionCount(positionCounts)
 }
 
 module.exports = validateLineup
