6599f89b3ab8bc1470591d2254b8ad970033ba59
Tricia Brooker <grimmi5894@gmail.com>
2021-05-24 18:34:24 -0400

    create functions to validate team count is not > 2

--- index.js
+++ index.js
@@ -4,12 +4,26 @@ const calculateTotalSalary = (lineup) => {
   }, 0)
 }
 
+const getTeamCount = (lineup) => {
+  return lineup.reduce((teamCounts, player) => {
+    teamCounts[player.teamId] = teamCounts[player.teamId] === undefined ? 1 : teamCounts[player.teamId]
+
+    return teamCounts
+  })
+}
+
 const violatesSalary = (lineup) => {
   return calculateTotalSalary(lineup) > 45000
 }
 
+const violatesTeamCount = (teamCounts) => {
+  return Object.values(teamCounts).some((count) => { return count > 2 })
+}
+
 const validateLineup = (lineup) => {
-  return !violatesSalary(lineup)
+  const teamCounts = getTeamCount(lineup)
+
+  return !violatesSalary(lineup) && !violatesTeamCount(teamCounts)
 }
 
 module.exports = validateLineup
