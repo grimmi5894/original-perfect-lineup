2ce6b5a8873872618bea3ef66b02a7414558664f
Tricia Brooker <grimmi5894@gmail.com>
2021-05-24 18:11:21 -0400

    creat function to validate salary amount under 45000

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-const validateLineup = () => {}
+const calculateTotalSalary = (lineup) => {
+  return lineup.reduce((salary, player) => {
+    return salary + player.salary
+  }, 0)
+}
+
+const violatesSalary = (lineup) => {
+  return calculateTotalSalary(lineup) > 45000
+}
+
+const validateLineup = (lineup) => {
+  return !violatesSalary(lineup)
+}
 
 module.exports = validateLineup
